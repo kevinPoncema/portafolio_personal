03bbdb8293c11405cf0151c83839387d8cd62c51
kevin@ponce <kevin.ponce@alumnos.uneatlantico.es>
2026-08-10 23:48:57 +0200

    chore: update site URL and remove base path in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kevinponcema.github.io',
-  base: '/portafolio_personal',
+  site: 'https://kevinponcedev.xyz',
   integrations: [tailwind()]
 });

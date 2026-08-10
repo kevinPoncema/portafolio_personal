c2e35a50a9f80177e7eb6700078b6e0f2897f65a
kevin@ponce <kevin.ponce@alumnos.uneatlantico.es>
2026-08-10 23:23:20 +0200

    feat: configure site URL and base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kevinponcema.github.io',
+  base: '/portafolio_personal',
   integrations: [tailwind()]
 });
